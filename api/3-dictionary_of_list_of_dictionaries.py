cfe279c0c09f57c64fb4c5d3d4bc16d5b10481ca
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-04-06 09:12:11 +0000

    task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -13,22 +13,25 @@ if __name__ == "__main__":
     r_users = requests.get("https://jsonplaceholder.typicode.com/users/")
     dict_users = r_users.json()
 
-    new_list = []
     new_dict2 = {}
 
-    for key in dict_users:
-        USER_ID = key.get("id")
-        USERNAME = key.get("username")
-        for k in dict_todos:
+    for employee in dict_users:
+        USER_ID = str(employee["id"])
+        USERNAME = employee["username"]
+        new_list = []
+        for task in dict_todos:
             new_dict = {}
-            if k.get("userId") == USER_ID:
-                new_dict["username"] = USERNAME
-                new_dict["task"] = k.get("title")
-                new_dict["completed"] = k.get("completed")
-                new_list.append(new_dict)
+            new_dict["username"] = USERNAME
+            new_dict["task"] = task["title"]
+            new_dict["completed"] = task["completed"]
+            new_list.append(new_dict)
+
         new_dict2[USER_ID] = new_list
 
-    json_object = json.dumps(new_dict2)
+    all_tasks = new_dict2
+
+    #json_object = json.dumps(new_dict2)
 
     with open("todo_all_employees.json", "w") as f:
-        f.write(json_object)
+       #f.write(json_object)
+       json.dump(all_tasks, f)
